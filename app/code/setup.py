8e016cf62097ab241272c1f0705d9a6ebd6c5650
= <mathieu.salaun12@gmail.com>
2021-07-31 15:03:13 +0200

    Starting use of setuptools
    *Working on setuptools to create a library of recount, which will be used
    *for the tests.

---
+++ app/code/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(name='recount',
+      version='1.0',
+      description='Python Distribution Utilities for Recount app',
+      author='Mathieu Salaün',
+      author_email='mathieu.salaun12@gmail.com',
+      packages=['wrapper_dash', 'wrapper_excel', 'wrapper_sql'],
+      requires=[
+            "dash",
+            "dash_auth",
+            "pandas",
+            "unidecode",
+            "stop_words",
+            "xlrd",
+            "pymysql",
+            "openpyxl",
+      ]
+     )
