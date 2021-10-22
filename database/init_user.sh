beb5e8a13f3205de960d0f89439c127239e32c3d
Raiytak <mathieu.salaun12@gmail.com>
2021-10-22 10:56:38 +0200

    Update of init_user.sh in db

--- database/init_user.sh
+++ database/init_user.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
   
-Q1="CREATE DATABASE IF NOT EXISTS depenses;"
-Q2="GRANT USAGE ON *.* TO myuser@localhost IDENTIFIED BY 'mypass';"
-Q3="GRANT ALL PRIVILEGES ON depenses.* TO myuser@localhost;"
+Q1="SOURCE init.sql;"
+Q2="CREATE USER 'myuser'@'localhost' IDENTIFIED BY 'mypass';"
+Q3="GRANT ALL PRIVILEGES ON expenses.* TO myuser@localhost;"
 Q4="FLUSH PRIVILEGES;"
-SQL="${Q1}${Q2}${Q3}${Q4}"
+SQL=$Q1$Q2$Q3$Q4
  
   
-mysql -uroot -proot -e "$SQL"
+sudo mysql -e "$SQL"
