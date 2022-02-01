4acc1e5fb1a8f87f11062e2a18f16f2639bd3f46
Raiytak <mathieu.salaun12@gmail.com>
2022-02-01 16:56:22 +0100

    Refactoring of app

--- recount/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd /home/$USER/Desktop/Projets/Recount/app
-virtualenv venv
-source venv/bin/activate
-pip install -r requirements.txt
-gunicorn -c gunicorn.conf.py "wsgi:create_app('production')"
-
--- recount/wsgi.py
+++
@@ -1,8 +0,0 @@
-# from dotenv import load_dotenv, find_dotenv
-# load_dotenv(find_dotenv())
-
-from src import app
-
-def create_app(environment):
-    dash_app = app.create_dash_app(environment)
-    return dash_app.app.server
