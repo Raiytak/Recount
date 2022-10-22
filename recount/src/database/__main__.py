b0bcc916f9f5c865071c92f61dea1c4d58692ff6
Raiytak <mathieu.salaun12@gmail.com>
2022-10-22 13:06:22 -0400

    Revision of database module, working

---
+++ recount/src/database/__main__.py
@@ -0,0 +1,42 @@
+import sys
+import argparse
+import logging
+
+import sql_db
+
+
+def testSqlConnection():
+    sql_table = sql_db.SqlTable()
+    print("**                       **")
+    print("   Connection successful   ")
+    print("Current database: '{}'".format(sql_table.database_name))
+    print("Current table: '{}'".format(sql_table.table_name))
+    print("Columns: '{}'".format(sql_table.columns_name))
+    print("**                       **")
+
+
+def testSqlTableExists():
+    # TODO
+    pass
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--test-sql-connection",
+    help="connect to the sql database. Raise Exception if fails",
+    action="store_true",
+    default=False,
+)
+parser.add_argument(
+    "--test-sql-table-exists",
+    help="check if the table provided exists on the current sql db",
+)
+
+args = parser.parse_args()
+
+if len(sys.argv) == 1:
+    parser.print_help()
+    sys.exit(1)
+
+if args.test_sql_connection:
+    testSqlConnection()
